d1fdd4c38cc82af9e0e65ec711ef0491d4d31255
jkosturko <jessica.kosturko@gmail.com>
2013-06-07 18:47:10 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+
+app.listen(80);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+
+  socket.on('circleCreated', function (data) {
+    socket.broadcast.emit('broadcastCircles', data);
+    console.log('circles', data);
+  });
+
+  socket.on('buttonClicked', function (data) {
+    //socket.emit('sendMessage', dataObj);
+    socket.broadcast.emit('sendMessage', data);
+  });
+});
